4902c844b5f91d4b675459e926b485088fb28631
Edozie <phynnedo@gmail.com>
2023-08-22 18:08:52 +0100

    fix: made some adjustments on the header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Header from './components/header/header';
+import SearchBar from './components/searchBar/searchBar';
 import './App.css';
 
 const App = () => {
 
   return (
-    <div>
+    <div className='w-100vw'>
       <Header/>
+      <section className='w-full bg-light-bg dark:bg-dark-bg'>
+        
+      </section>
     </div>
   )
 }
---
+++ src/components/searchBar/searchBar.tsx
@@ -0,0 +1,12 @@
+
+
+
+const SearchBar = () => {
+    return (
+        <form>
+            
+        </form>
+    );
+}
+ 
+export default SearchBar;
