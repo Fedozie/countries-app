27969c0306d45b551fa9754f54406af58701a38f
Edozie <phynnedo@gmail.com>
2023-08-22 09:51:51 +0100

    feat: added the header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,27 @@
-
-
-
 const Header = () => {
-    return (
-        <header className="w-100vw h-4/5 flex flex-row justify-between items-center py-4 px-20">
-            <div className="">Where in the world?</div>
-            <div className="flex">
-                <div className="mr-4">moon icon</div>
-                <div>Dark Mode</div>
-            </div>
-        </header>
-    );
-}
- 
+  return (
+    <header className="w-100vw h-4/5 flex flex-row justify-between items-center py-4 px-20 shadow">
+      <div className="font-extrabold text-xl text-light-text">Where in the world?</div>
+      <div className="flex">
+        <svg
+          className="w-6 h-6 mr-4 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 18 20"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M8.509 5.75c0-1.493.394-2.96 1.144-4.25h-.081a8.5 8.5 0 1 0 7.356 12.746A8.5 8.5 0 0 1 8.509 5.75Z"
+          />
+        </svg>
+        <div className="font-semibold text-light-text">Dark Mode</div>
+      </div>
+    </header>
+  );
+};
+
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,27 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    colors: {
+      'dark-els': 'hsl(209, 23%, 22%)',
+      'dark-bg': 'hsl(207, 26%, 17%)',
+      'dark-text': 'hsl(0, 0%, 100%)',
+      'light-text': 'hsl(208, 15%, 8%)',
+      'light-input': 'hsl(0, 0%, 52%)',
+      'light-bg': 'hsl(0, 0%, 98%)',
+    },
+    
+    extend: {
+      fontFamily: {
+        'primary': ['Nunito Sans']
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
