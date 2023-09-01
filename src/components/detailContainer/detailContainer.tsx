8769e5fe303f5419441132fcf3c753119f895d57
Edozie <phynnedo@gmail.com>
2023-09-01 12:46:15 +0100

    fix: added the dark theme to the details page

--- src/components/detailContainer/detailContainer.tsx
+++ src/components/detailContainer/detailContainer.tsx
@@ -9,7 +9,7 @@ const DetailContainer = ({ country, handleNavigate}: CountryProps) => {
   return (
     <div className="w-full">
       <div className="w-full mb-12">
-        <button className="w-36 p-3 bg-light-bg shadow-2xl rounded-lg flex justify-center items-center" onClick = {handleNavigate}> 
+        <button className="w-36 p-3 bg-light-bg shadow-2xl rounded-lg flex justify-center items-center dark:bg-dark-els" onClick = {handleNavigate}> 
           <svg
             className="w-6 h-6 text-gray-800 dark:text-white"
             aria-hidden="true"
@@ -25,7 +25,7 @@ const DetailContainer = ({ country, handleNavigate}: CountryProps) => {
               d="M13 5H1m0 0 4 4M1 5l4-4"
             />
           </svg>
-          <p className="ml-2">Back</p>
+          <p className="ml-2 dark:text-dark-text">Back</p>
         </button>
       </div>
       <div className="w-full flex gap-32">
@@ -33,56 +33,56 @@ const DetailContainer = ({ country, handleNavigate}: CountryProps) => {
           <img src={country.flags.svg} alt="Country's Flag" />
         </div>
         <div className="w-1/2 ">
-          <h3 className="text-3xl font-semibold mb-4 text-light-text">
+          <h3 className="text-3xl font-semibold mb-4 text-light-text dark:text-dark-text">
             {country.name}
           </h3>
           <div className="flex gap-14">
             <div>
-              <p className="mb-2">
-                <span className="text-light-text font-semibold">
+              <p className="mb-2 dark:text-dark-text">
+                <span className="text-light-text font-semibold dark:text-dark-text">
                   Native Name:{" "}
                 </span>
                 {country.nativeName}
               </p>
-              <p className="mb-2 text-dark-bg">
-                <span className="text-light-text font-semibold">
+              <p className="mb-2 text-dark-bg dark:text-dark-text">
+                <span className="text-light-text font-semibold dark:text-dark-text">
                   Population:{" "}
                 </span>
                 {country.population.toLocaleString()}
               </p>
-              <p className="mb-2 text-dark-bg">
-                <span className="text-light-text font-semibold">Region: </span>
+              <p className="mb-2 text-dark-bg dark:text-dark-text">
+                <span className="text-light-text font-semibold dark:text-dark-text">Region: </span>
                 {country.region}
               </p>
-              <p className="mb-2 text-dark-bg">
-                <span className="text-light-text font-semibold">
+              <p className="mb-2 text-dark-bg dark:text-dark-text">
+                <span className="text-light-text font-semibold dark:text-dark-text">
                   Sub Region:{" "}
                 </span>
                 {country.subregion}
               </p>
-              <p className="mb-2 text-dark-bg">
-                <span className="text-light-text font-semibold">Capital: </span>
+              <p className="mb-2 text-dark-bg dark:text-dark-text">
+                <span className="text-light-text font-semibold dark:text-dark-text">Capital: </span>
                 {country.capital}
               </p>
             </div>
             <div>
-              <p className="mb-2 text-dark-bg">
-                <span className="font-semibold">Top Level Domain: </span>
+              <p className="mb-2 text-dark-bg dark:text-dark-text">
+                <span className="font-semibold dark:text-dark-text">Top Level Domain: </span>
                 {country.topLevelDomain}
               </p>
               <div className="flex gap-1 mb-2">
-              <span className="font-semibold">Currency: </span>
+              <span className="font-semibold dark:text-dark-text">Currency: </span>
               {country.currencies.map((currency: any, index: any) => (
-                <p key={index}>
+                <p key={index} className = "dark:text-dark-text">
                   {currency.name}
                 </p>
               ))}
               </div>
               
               <div className="flex gap-1">
-                <span className="mb-2 text-dark-bg font-semibold">Langauage:</span>
+                <span className="mb-2 text-dark-els font-semibold dark:text-dark-text">Langauage:</span>
                 {country.languages.map((language: any, index: any) => (
-                    <p key = {index}>
+                    <p key = {index} className="dark:text-dark-text">
                      {language.name},
                     </p>
               ))}
@@ -91,11 +91,11 @@ const DetailContainer = ({ country, handleNavigate}: CountryProps) => {
             </div>
           </div>
           <div className="mt-20 flex justify-start items-center">
-            <p className="font-semibold text-dark-bg">Border Countries: </p>
+            <p className="font-semibold text-dark-els dark:text-dark-text">Border Countries: </p>
             {country.borders ?
               country.borders.map((border: string, index: any) => (
-                <span className="w-12 rounded shadow m-2 p-1 flex justify-center items-center" key ={index}>{border}</span>
-              )) : <span className="ml-1">No Border Countries</span>}
+                <span className="w-12 rounded shadow m-2 p-1 flex justify-center items-center dark:text-dark-text" key ={index}>{border}</span>
+              )) : <span className="ml-1 dark:text-dark-text">No Border Countries</span>}
           </div>
         </div>
       </div>
